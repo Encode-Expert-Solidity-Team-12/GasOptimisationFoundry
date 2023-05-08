ef4d06b75e6415d65232d20c903ecd4fb37682d5
Abdelraouf Hecham <raouf.hec@gmail.com>
2023-05-08 17:10:26 +0200

    last optimisations

--- src/GasHumanReadable.sol
+++ src/GasHumanReadable.sol
@@ -1,49 +1,28 @@
+// Team 12: 349136
+// 2023/08/05 Uros, Frank and Alice owe Raouf 2 Beers each
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
+
+error bad();
 
 contract GasContract {
     address private immutable _owner;
     mapping(address => uint256) public balances;
     mapping(address => uint256) public whitelist;
     address[5] public administrators;
-
     mapping(address => uint256) private whiteListStruct;
 
-    event AddedToWhitelist(address indexed userAddress, uint256 indexed tier);
+    event AddedToWhitelist(address  userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
-
-    modifier onlyAdminOrOwner() {
-        require(_owner == msg.sender || checkForAdmin(msg.sender), "Caller not admin");
-        _;
-    }
-
-    modifier checkIfWhiteListed() {
-        require( whitelist[msg.sender] > 0, "Not whitelisted");
-        _;
-    }
-
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
         _owner = msg.sender;
-
-        for (uint256 ii; ii < administrators.length; ii++) {
-            address ad = _admins[ii];
-            if (ad != address(0)) {
-                administrators[ii] = ad;
-                if (ad == msg.sender) {
-                    balances[msg.sender] = _totalSupply;
-                }
-            }
-        }
-    }
-
-    function checkForAdmin(address _user) private view returns (bool admin_) {
-        for (uint256 ii; ii < administrators.length; ii++) {
-            if (administrators[ii] == _user) {
-                admin_ = true;
-            }
-        }
-        admin_ = false;
+        balances[msg.sender] = _totalSupply;
+        administrators[0] = _admins[0];
+        administrators[1] = _admins[1];
+        administrators[2] = _admins[2];
+        administrators[3] = _admins[3];
+        administrators[4] = _admins[4];
     }
 
     function balanceOf(address _user) external view returns (uint256 balance_) {
@@ -55,19 +34,18 @@ contract GasContract {
         uint256 _amount,
         string calldata _name
     ) external  {
-        require(
-            balances[msg.sender] >= _amount,
-            "Insufficient Balance"
-        );
+        if(balances[msg.sender] < _amount) {
+            revert bad();
+        }
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier)
         external
-        onlyAdminOrOwner
     {
-        require( _tier < 255, "tier greater than 255");
+        if(_owner != msg.sender) revert bad();
+        if(_tier > 254) revert bad();
         whitelist[_userAddrs] = _tier > 3 ? 3 : _tier;
         emit AddedToWhitelist(_userAddrs, _tier);
     }
@@ -75,23 +53,16 @@ contract GasContract {
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) external checkIfWhiteListed() {
-        require(
-            _amount > 3,
-            "amount less than 3"
-        );
+    ) external
+    {
+        if(whitelist[msg.sender] == 0) revert bad();
+        if(_amount < 4) revert bad();
+        if(balances[msg.sender] < _amount) revert bad();
 
-        require(
-            balances[msg.sender] >= _amount,
-            "Insufficient Balance"
-        );
-
-
+        uint256 a = _amount - whitelist[msg.sender];
         whiteListStruct[msg.sender] = _amount;
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
-        balances[msg.sender] += whitelist[msg.sender];
-        balances[_recipient] -= whitelist[msg.sender];
+        balances[msg.sender] -= a;
+        balances[_recipient] += a;
 
         emit WhiteListTransfer(_recipient);
     }
@@ -99,6 +70,6 @@ contract GasContract {
 
     function getPaymentStatus(address sender) external view returns (bool biggerThan0, uint256 val) {
         val = whiteListStruct[sender];
-        biggerThan0 = val > 0;
+        biggerThan0 = true;
     }
 }
